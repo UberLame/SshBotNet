009adbc0cd274f71a8a1d23c0f1435ad5cb53721
UberLame <w0rm@sploit.me.uk>
2013-11-22 11:00:12 -0600

    class setup

--- worker.py
+++ worker.py
@@ -1,2 +1,27 @@
 #!/usr/bin/python
 
+import optparse
+import pxssh
+
+class Client:
+	def __init__(self, host, user, password):
+		self.host = host
+		self.user = user
+		self.password = password
+		self.session = self.connect()
+
+	def connect(self):
+		try:
+			s = pxssh.pxssh()
+			s.login(self.host, self.user, self.password)
+			return s
+		except Exception, e:
+			print e
+			print '[-] Error Connecting to Host: ' + self.host + '\r'
+
+	def send_command(self, cmd):
+		self.session.sendline(cmd)
+		self.session.prompt()
+		return self.session.before
+
+
